9b140dcb8a8c1b93cc785417a93b5e0c8f94a8d0
momchil <momchil@momchils-MacBook-Air.local>
2022-05-02 09:49:10 +0200

    import allProjects in App.js and as a props an ShowAllProjects gave

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import ShowAllProjects from './components/ShowAllProjects';
+import allProjects from './db';
 
 function App() {
     return (
         <div className="App">
-            <ShowAllProjects />
+            <ShowAllProjects  allProjects= {allProjects}/>
         </div>
     );
 }
--- client/src/components/ShowAllProjects.jsx
+++ client/src/components/ShowAllProjects.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
-import allProjects from "../db";
 
 
-function ShowAllProjects(){
+
+function ShowAllProjects({allProjects}){
     
     return (
         <>
