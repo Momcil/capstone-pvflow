a881b5e2622448b7c7cf55031fa9eafc9e7be696
momchil <momchil@momchils-MacBook-Air.local>
2022-05-03 10:27:17 +0200

    add toggle function to show details

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import ShowAllProjects from './components/ShowAllProjects';
 import allProjects from './db';
 
+
 function App() {
+
     return (
         <div className="App">
             <ShowAllProjects  allProjects= {allProjects}/>
--- client/src/components/ShowAllProjects.jsx
+++ client/src/components/ShowAllProjects.jsx
@@ -1,14 +1,23 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 
+export default function ShowAllProjects({allProjects}){
+    const [details, setDetails] = useState([])
 
-function ShowAllProjects({allProjects}){
-    
+    function Toggle(projectID){
+        const newDetails = [...allProjects]
+        newDetails[projectID].in_detail = newDetails[projectID].in_detail ? false : true
+        setDetails(newDetails)
+        console.log(newDetails)
+    }
+ 
     return (
         <>
             <h1>Projects List</h1>
             <Container> 
-                {allProjects.map((project) => 
+                {allProjects.map((project) =>    
+                    project.in_detail ?            
                     <ProjectCard role="list" key={project.ID}>
                         <p>Project ID: {project.ID}</p>
                         <p>Client: {project.client}</p>
@@ -17,14 +26,26 @@ function ShowAllProjects({allProjects}){
                         <p>Start date: {project.start_date}</p>
                         <p> End date: {project.end_date}</p>
                         <p>Sum of work-hours: <span>{project.sum_hours}</span></p>
-                    </ProjectCard>
+                        <button className="btn" id={project.ID} onClick={() => Toggle(project.ID - 1)}>
+                                {project.hasDetails ? "close" : "show more"}
+                        </button>
+                    </ProjectCard> 
+                    :
+                    <ProjectCard role="list" key={project.ID}>
+                        <p>Project ID: {project.ID}</p> 
+                        <p>Client: {project.client}</p>
+                        <p>Status: {project.status}</p>
+                        <button className="btn" id={project.ID} onClick={() => Toggle(project.ID - 1)}>
+                            {project.hasDetails ? "close" : "show more"}
+                        </button>
+                    </ProjectCard> 
                 )}  
             </Container> 
         </>    
     )
 }    
 
-export default ShowAllProjects
+
 
 
 const ProjectCard = styled.li`
