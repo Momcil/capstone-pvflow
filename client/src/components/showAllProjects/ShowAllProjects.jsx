f94b4544ea221e21beb609f5e66eb588064f6cf4
momchil <momchil@momchils-MacBook-Air.local>
2022-05-23 14:58:53 +0200

    haha ++

--- client/src/components/showAllProjects/ShowAllProjects.jsx
+++ client/src/components/showAllProjects/ShowAllProjects.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useState } from "react";
+// import { useState } from "react";
 import Button from "../button/ShowMoreButton";
 import DeleteButton from "../button/DeleteButton";
 
